4908dc3e269f3ef9e02f12f81476aaab669e7ab1
Mihailo Djuric <mihailo.djuric@kodit.rs>
2022-10-14 11:25:50 +0200

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -23,24 +17,21 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-            
-            
-            
         }
-
-        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-                
-            } );
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
 
-            services.AddControllers();
+            services
+                .AddControllers()
+                .AddNewtonsoftJson();
+                
             services.AddCors();
+            
+            
 
             services.AddSwaggerGen(c =>
             {
@@ -59,9 +50,11 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+        
 
             app.UseRouting();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
