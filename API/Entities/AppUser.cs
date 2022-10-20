9eec420c7ba56f90faa081cf1b7f524a614da719
Mihailo Djuric <mihailo.djuric@kodit.rs>
2022-10-20 15:57:51 +0200

    section 14 almost done

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,6 +8,7 @@ namespace API.Entities
 {
     public class AppUser
     {
+        
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
@@ -23,12 +24,10 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
 
-      //  public int GetAge()
-       // {
-
-         //   return DateOfBirth.CalculateAge();
-       // }
+      
 
     }
 }
